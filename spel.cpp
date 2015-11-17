8635d3482d499afd8c3e40d525f4ae84f80a518b
Anton Kindestam <bountyjedi@gmail.com>
2015-11-17 11:52:47 +0100

    splitting out into header files

--- spel.cpp
+++ spel.cpp
@@ -9,83 +9,7 @@ class ItemOwner;
 class Room;
 
 
-class Item
-{
-private:
-    ItemOwner *owner;
-    std::string name;
-    std::string description;
-    unsigned weight;
-public:
 
-    Item(std::string nam, std::string descriptio, unsigned weigh) : owner(nullptr), name(nam), description(descriptio), weight(weigh)
-    {
-    }
-
-    const std::string& getName() const
-    {
-        return name;
-    }
-
-    const std::string& getDescription() const
-    {
-        return description;
-    }
-
-    unsigned getWeight() const
-    {
-        return weight;
-    }
-
-    friend class ItemOwner;
-};
-
-
-class ItemOwner 
-{
-protected:
-    virtual bool can_carry(const Item* item) const
-    {
-        return true;
-    }
-    
-    std::list<Item*> items;
-
-public:
-    bool add_item(Item *i)
-    {
-        // TODO: or use exception
-        if (!this->can_carry(i))
-            return false;
-                
-        items.push_front(i);
-        if (i->owner != nullptr) // items can be ownerless (though normally only directly after being constructed
-            i->owner->removeItem(i);
-        i->owner = this;
-
-        return true;
-    }
-    
-    void removeItem(Item* i)
-    {
-        // TODO: instead of returning bool throw exception when it doesn't exist
-        
-        auto it = std::find(items.begin(), items.end(), i);
-        if (it == items.end())
-        {
-            // TODO: THROW EXCEPTION
-        }
-        items.erase(it);
-    }
-
-    virtual ~ItemOwner()
-    {
-        std::cerr << "ItemOwner destructor" << std::endl;
-        
-        for (Item *i: items)
-            delete i;
-    }
-};
 
 class Actor : public ItemOwner 
 {
@@ -166,6 +90,7 @@ class Player : public Actor
 {
 private:
     static unsigned magisk_konstant = 10;
+    unsigned strength;
 protected:
     virtual bool can_carry(const Item *i) const override
     {
@@ -175,8 +100,6 @@ protected:
         total_weight += i->getWeight();
         return (total_weight > strength*magisk_konstant);
     }
-private:
-    unsigned strength;
 public:
     
 };
