eb4f7e531026d84787aa5614372d33eaa5380d4d
Anton Kindestam <bountyjedi@gmail.com>
2015-11-17 13:41:43 +0100

    Player compiles

--- Player.hpp
+++ Player.hpp
@@ -1,16 +1,19 @@
 #ifndef PLAYER_HPP
 #define PLAYER_HPP
+
 #include "Actor.hpp"
-#include <stdlib>
+#include "Item.hpp"
 #include <iostream>
+
 class Player : public Actor
 {
 private:
     unsigned strength;
-    static unsigned magisk_konstant = 10;
+    const static unsigned magisk_konstant = 10;
 protected:
     virtual bool can_carry(const Item *i) const override;
 public:
     
 };
+
 #endif
