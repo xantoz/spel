df38634c8ed6efbf8352ce0962c5f2bc05fba67e
Anton Kindestam <bountyjedi@gmail.com>
2015-11-17 12:57:50 +0100

    missade att ta bort skrap i player.hpp

--- Player.hpp
+++ Player.hpp
@@ -9,15 +9,7 @@ private:
     unsigned strength;
     static unsigned magisk_konstant = 10;
 protected:
-    virtual bool can_carry(const Item *i) const override
-    {
-        unsigned total_weight = 0;
-        for(Item *i: items)
-            total_weight += i->getWeight();
-        total_weight += i->getWeight();
-        return (total_weight > strength*magisk_konstant);
-    }
-
+    virtual bool can_carry(const Item *i) const override;
 public:
     
 };
