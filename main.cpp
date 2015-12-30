2b720f387e6e98b0fa07bcabbed16d11b70f5c6b
Kai Widell Niigata <kaiwn@kth.se>
2015-12-30 14:36:17 +0100

    ändrade main till map av funktionspekare

--- main.cpp
+++ main.cpp
@@ -7,13 +7,78 @@
 #include <cstring>
 #include <cstdio>
 #include <iostream>
-
+#include <functional>
 #include <boost/algorithm/string/trim_all.hpp>
 
 using namespace std;
+Player* player;
+
+void go(string arg)
+{
+     if (arg == "")
+     {
+         cout << "Where did you want to go, you said?" << endl;
+         return;
+     }
+     player->go(arg);
+     cout << player->getRoom()->getName() << endl;
+     cout << player->getRoom()->getDescription() << endl;
+}
+
+void look(string arg)
+{
+ // cout << player->getRoom()->getDescription() << endl
+    if (arg == "")
+        cout << player->look() << endl;
+    else
+        cout << player->look(arg) << endl;    
+}
+
+void use(string arg)
+{
+    if (arg == "")
+    {
+        cout << "What do you want to use?" << endl;
+        return;
+    }
+    cout << player->use(arg) << endl;   
+}
+
+void pickup(string arg)
+{
+     if (arg == "")
+     {
+         cout << "What did you want to pick up?" << endl;
+         return;
+     }
+     if (player->pickup(arg))
+         cout << "Picked up " << arg << endl;
+     else
+         cout << "I don't see any such thing." << endl;
+}
+
+void drop(string arg)
+{
+    if (arg == "")
+    {
+        cout << "What did you want to drop?" << endl;
+        return;
+    }
+    if (player->drop(arg))
+        cout << "Dropped " << arg << endl;
+    else
+        cout << "I'm not carrying anything like that." << endl;
+}
 
 int main(int argc, char** argv)
 {
+    std::map<string, function<void(string)> > map;
+    map["go"] = &go;
+    map["look"] = &look;
+    map["use"] = &use;
+    map["pickup"] = &pickup;
+    map["drop"] = &drop;
+    
     Room* kitchen = new Room("Kitchen", "This is the kitchen", nullptr);
     Room* first = new Room("My room", "This is my room", "west", kitchen, nullptr);
     kitchen->setExit("east", first);
@@ -21,12 +86,12 @@ int main(int argc, char** argv)
     kitchen->addItem(new Sword("dagger", "It's just your normal dagger.", 10, daggerstats));
 
     Stats nilsstats = { 20, 10, 10, 10, 10, 10 };
-    Player* player = new Player("nils", "it's you", nilsstats);
+    player = new Player("nils", "it's you", nilsstats);
     first->addActor(player);
 
     cout << player->getRoom()->getName() << endl;
     cout << player->getRoom()->getDescription() << endl;
-    std::string str;
+    string str;
     while(!cin.eof())
     {
         try 
@@ -38,65 +103,13 @@ int main(int argc, char** argv)
             size_t first_space = str.find_first_of(' ');
             string command = str.substr(0, first_space);
             string arg = (first_space == string::npos) ? "" : str.substr(first_space + 1);
-
+            map.at(command)(arg);
+            
             // cout << "\"" << command << "\" \"" << arg << "\"" << endl;
-
-            if (command == "go")
-            {
-                if (arg == "")
-                {
-                    cout << "Where did you want to go, you said?" << endl;
-                    continue;
-                }
-                player->go(arg);
-                cout << player->getRoom()->getName() << endl;
-                cout << player->getRoom()->getDescription() << endl;
-            }
-            else if (command == "look")
-            {
-                // cout << player->getRoom()->getDescription() << endl;
-                if (arg == "")
-                    cout << player->look() << endl;
-                else
-                    cout << player->look(arg) << endl;
-            }
-            else if (command == "use")
-            {
-                if (arg == "")
-                {
-                    cout << "What do you want to use?" << endl;
-                    continue;
-                }
-                cout << player->use(arg) << endl;
-            }
-            else if (command == "pickup")
-            {
-                if (arg == "")
-                {
-                    cout << "What did you want to pick up?" << endl;
-                    continue;
-                }
-                if (player->pickup(arg))
-                    cout << "Picked up " << arg << endl;
-                else 
-                    cout << "I don't see any such thing." << endl;
-            }
-            else if (command == "drop")
-            {
-                if (arg == "")
-                {
-                    cout << "What did you want to drop?" << endl;
-                    continue;
-                }
-                if (player->drop(arg))
-                    cout << "Dropped " << arg << endl;
-                else
-                    cout << "I'm not carrying anything like that." << endl;
-            }
-            else
-            {
-                cout << "What are ye doing, LOL" << endl;
-            }
+        }
+        catch(const out_of_range &e)
+        {
+            cout << "What are ye doing, LOL" << endl;
         }
         catch (const GameException &e)
         {
