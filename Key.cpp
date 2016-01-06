aa315ae02660b11adff7cd95188572a667ee8179
Anton Kindestam <bountyjedi@gmail.com>
2016-01-06 17:44:07 +0100

    Key more filledout-ish. Serialization and loading still incomplete

--- Key.cpp
+++ Key.cpp
@@ -1,5 +1,59 @@
 #include "Key.hpp"
-Key::Key(const std::string &name, const std::string &description, unsigned weight) :
-    Item(name, description, weight) 
+
+#include "Serialize.hpp"
+#include "Room.hpp"
+#include <iostream>
+
+Key::Key(const std::string &name,
+         const std::string &description,
+         unsigned weight) :
+    Key(name, description, weight, nullptr, "", nullptr, "")
 {
+}
+
+Key::Key(const std::string &name,
+         const std::string &description,
+         unsigned weight,
+         Room *fr, const std::string &frd,
+         Room *tr, const std::string &trd) :
+    Item(name, description, weight),
+    fromRoom(fr),
+    fromRoomDirection(frd),
+    toRoom(tr),
+    toRoomDirection(trd)
+{
+    consumable = true;
+}
+
+Key::~Key()
+{
+    std::cerr << "Key<" << getName() << "> destructor" << std::endl;
+}
+
+void Key::setAction(Room *fr, const std::string &frdir,
+                    Room* tr, const std::string &trdir)
+{
+    fromRoom = fr;
+    fromRoomDirection = frdir;
+    toRoom = tr;
+    toRoomDirection = trdir;
+}
+
+std::string Key::use(Actor *actor)
+{
+    return "";
+}
+
+std::string Key::serialize(std::ostream &os)
+{
+    // Due to how the file format is made we might need to refer to yet
+    // non-created rooms and thus the constructor used in the save-file
+    // format doesn't take the last four parameters, instead opting to set
+    // them at a later time.
+    std::string sym = gensym();
+    os << sym << ":MAKE-KEY "
+       << stringify(getName()) << " "
+       << stringify(getBaseDescription()) << " "
+       << getWeight() << std::endl;
+    return sym;
 }
--- Key.hpp
+++ Key.hpp
@@ -1,10 +1,37 @@
 #ifndef KEY_HPP
 #define KEY_HPP
+
 #include "Item.hpp"
+#include <ostream>
+#include <string>
+
+class Room;
 class Key : public Item 
 {
+private:
+    Room *fromRoom;
+    std::string fromRoomDirection;
+    Room *toRoom;
+    std::string toRoomDirection;
+
+    // KLUDGE: hack to keep track of key syms
+    
+    
 public:
-    Key(const std::string &name, const std::string &description, unsigned weight);
+    Key(const std::string &name,
+        const std::string &description,
+        unsigned weight);
+    Key(const std::string &name,
+        const std::string &description,
+        unsigned weight,
+        Room *fromRoom, const std::string &fromRoomDirection,
+        Room *toRoom, const std::string &toRoomDirectionx);
+    virtual ~Key() override;
+
+    void setAction(Room *fromRoom, const std::string &fromRoomDirection,
+                   Room* toRoom, const std::string &toRoomDirection);
+    virtual std::string use(Actor *actor) override;
+    virtual std::string serialize(std::ostream &os) const override;
+};
     
-}
-    
+#endif
