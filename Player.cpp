eefcc937c2e885f2a1b25a0bc755c163e36f9d43
Anton Kindestam <bountyjedi@gmail.com>
2016-01-22 17:42:55 +0100

    make it possible to look at items in your inventory too

--- Player.cpp
+++ Player.cpp
@@ -49,25 +49,22 @@ std::string Player::look(const std::string &actorOrItem) const
     std::string ret = "";
 
     if (getRoom() == nullptr) return ret;
-    if (actorOrItem == "self")
+    if (actorOrItem == "self" || actorOrItem == "myself")
     {
         return getDescription();
     }
     
-    Actor *a = getRoom()->getActor(actorOrItem);
-    Item *i = getRoom()->getItem(actorOrItem);
-    if (a != nullptr)
-    {
-        return a->getDescription();
-    }
-    else if (i != nullptr)
-    {
-        return i->getDescription();
-    }
+    Actor *actor = getRoom()->getActor(actorOrItem);
+    Item *item = getRoom()->getItem(actorOrItem);
+    Item *inventory_item = this->getItem(actorOrItem);
+    if (actor != nullptr)
+        return actor->getDescription();
+    else if (item != nullptr)
+        return item->getDescription();
+    else if (inventory_item != nullptr)
+        return inventory_item->getDescription();
     else
-    {
         return "I don't see any " + actorOrItem + ".";
-    }
 }
 
 void Player::attackResponse(Actor *actor)
