70ede24fbcb138a811145f9e0f5195dff8dd6322
Anton Kindestam <bountyjedi@gmail.com>
2016-02-09 15:17:15 +0100

    basic completion of commands

--- main.cpp
+++ main.cpp
@@ -15,6 +15,8 @@
 #include <iostream>
 #include <fstream>
 #include <functional>
+#include <memory>
+
 #include <boost/algorithm/string/trim_all.hpp>
 
 #include <readline/readline.h>
@@ -31,6 +33,10 @@ Player *player;
 Actor *opponent;
 CallbackHuman *finalBoss;
 Shop *shop;
+
+std::map<const string, function<void(string)>> cmds;
+std::map<const string, function<void(string)>> battleCmds;
+std::map<const string, function<void(string)>> shopCmds(cmds);
 
 void destroy_everything()
 {
@@ -523,10 +529,92 @@ void kill(string arg)
 }
 
 
+void *xmalloc(int size)
+{
+    void *buf;
+ 
+    buf = malloc(size);
+    if (!buf)
+    {
+        fprintf(stderr, "Error: Out of memory. Exiting.'n");
+        exit (1);
+    }
+ 
+    return buf;
+}
+
+char *dupstr (const char* s)
+{
+    char *r;
+ 
+    r = (char*)xmalloc((strlen (s) + 1));
+    strcpy(r, s);
+    return r;
+}
+
+template<class Whatever>
+static char const **cmd_from_map(const map<const string, Whatever> &map)
+{
+    char const **cmd = new const char*[(map.size() + 1)];
+    char const **p = cmd;
+    for (auto &item: map)
+        *p++ = item.first.c_str();
+    *p = nullptr;
+
+    return cmd;
+}
+
+char *my_generator(const char* text, int state)
+{
+    // char* cmd [] = {"hello", "world", "hell" ,"word", "quit", NULL};
+    unique_ptr<const char*[]> cmd(cmd_from_map((mode == BATTLE) ? battleCmds :
+                                               (mode == SHOP)   ? shopCmds : cmds));
+    
+    static int list_index, len;
+    const char *name;
+ 
+    if (!state)
+    {
+        list_index = 0;
+        len = strlen(text);
+    }
+ 
+    while (nullptr != (name = cmd[list_index]))
+    {
+        list_index++;
+ 
+        if (strncmp(name, text, len) == 0)
+            return (dupstr(name));
+    }
+ 
+    /* If no names matched, then return NULL. */
+    return ((char *)NULL);
+}
+
+static char **my_completion(const char *text , int start,  int end)
+{
+    char **matches;
+ 
+    matches = (char **)NULL;
+
+    // cout << "DERP:" << start << endl;
+    // cout << "HERP:" << text << endl;
+    // cout << "MERP:" << rl_line_buffer << endl;
+ 
+    if (start == 0)
+        matches = rl_completion_matches((char*)text, &my_generator);
+    else if(false)
+    {
+    }
+    else
+        rl_bind_key('\t',rl_abort);
+    
+    return matches;
+}
+
 int main(int argc, char** argv)
 {
-    std::map<string, function<void(string)>> cmds;
-    std::map<string, function<void(string)>> battleCmds;
+
 
     cmds["go"] = &go;
     cmds["look"] = &look;
@@ -552,7 +640,7 @@ int main(int argc, char** argv)
     battleCmds["kill"] = &kill;
 
     // shopCmds consists of the normal cmds plus a few extra, minus saving and loading
-    std::map<string, function<void(string)>> shopCmds(cmds);
+    shopCmds = cmds;
     shopCmds.erase("save");
     shopCmds.erase("load");
     shopCmds["list"] = &listItems;
@@ -566,7 +654,9 @@ int main(int argc, char** argv)
     ifs.close();
     string str;
     char *inpt;
-    while(nullptr != (inpt = readline("\n> ")))
+
+    rl_attempted_completion_function = my_completion;
+    while (nullptr != (inpt = readline("\n> ")))
     {
         rl_bind_key('\t',rl_complete);
         add_history(inpt);
