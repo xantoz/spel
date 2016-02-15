324551b64cc80988388547e38acc6ca40fd353d7
Kai Widell Niigata <kaiwn@kth.se>
2016-02-15 11:07:15 +0100

    Classes debug message update Changed weights and spawn rates

--- Classes.cpp
+++ Classes.cpp
@@ -34,7 +34,8 @@ static void updateActor(Actor* actor)
         {
             if(dynamic_cast<Shop*>(roomDirection.second) == nullptr)
             {
-                std::cerr << actor->getName() << " moved to " << roomDirection.first << std::endl;
+                std::cerr << actor->getName() << " moved from " << actor->getRoom()->getName() << " to " << roomDirection.first << std::endl;
+                
                 roomDirection.second->addActor(actor);
                 //std::cout << "not broken" << std::endl;
             }
